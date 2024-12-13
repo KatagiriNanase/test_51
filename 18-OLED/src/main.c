828f966ed8db8c0f7172a3369320c329d48aa25a
xinhaitianyu <shicao33@gmail.com>
2024-12-13 21:53:35 +0800

    51 day12

--- 18-OLED/src/main.c
+++ 18-OLED/src/main.c
@@ -3,6 +3,9 @@ void main()
 {
     Int_OLED_Init();
     Int_OLED_Clear();
+    Int_OLED_ShowStr(0, 0, " You woke me up");
+    Int_OLED_ShowStr(4, 2, "\"Master\"");
+    Int_OLED_ShowNum(4, 3, -134465);
     while (1)
     {
 
